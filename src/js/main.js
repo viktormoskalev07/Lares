656459195d13b14c304a6ed375aaca36deca7e15
Viktor Moskalev <viktormoskalev07@gmail.com>
2021-09-27 14:10:57 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+"use strict"
+const body =document.querySelector("body");
+const html=document.querySelector("html");
+const header=document.querySelector(".header");
+const navBtn=document.querySelector(".nav__btn");
+ 
+   
+
+ 
+      @@include('modules/nav.js') 
+      @@include('modules/fixed-header.js')
+  
+      // @@include('lazyscripts.js')
+      // @@include('modules/accordion.js')
+
+ 
+
+
+
